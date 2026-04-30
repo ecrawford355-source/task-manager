77b796633978e677feb552853accf2aa973b5b65
ecrawford355-source <ecrawford355@gmail.com>
2026-04-30 13:13:21 -0700

    Add Board model

---
+++ backend/src/models/Board.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const boardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    default: ''
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  members: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  columns: [{
+    _id: mongoose.Schema.Types.ObjectId,
+    name: String,
+    order: Number
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Board', boardSchema);
